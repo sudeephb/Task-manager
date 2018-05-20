c0eb2e15c21dd855afa796b2baaf83c1c95ef03d
Sudeep Bhandari <sudeephb19@gmail.com>
2018-05-20 19:37:49 +0545

    added status on Task model

--- task_manager/todo/models.py
+++ task_manager/todo/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
+STATUS_CHOICES = (
+    ('com','complete'),
+    ('incom','incomplete'),
+    ('to','todo'),
+)
+
 # Create your models here.
 
 class Task(models.Model):
 	name = models.CharField(max_length = 128)
 	due = models.DateTimeField(blank = True, null = True)
+	status = models.CharField(max_length = 16, choices = STATUS_CHOICES, default = 'to', blank = False, null = False)
+
 	def __str__(self):
 		return (f"{self.name}")
