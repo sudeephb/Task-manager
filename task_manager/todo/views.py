4de81a1f37c70bb6464750a43aa324c385d96185
Sudeep Bhandari <sudeephb19@gmail.com>
2018-05-31 16:24:22 +0545

    Refactored,

--- task_manager/todo/views.py
+++ task_manager/todo/views.py
@@ -5,8 +5,30 @@ from .models import Task
 
 
 def tasks(request):
+	tasks = Task.objects.all()	
+	todoTasks = []
+	for task in tasks:
+		if task.status == 'to':
+			todoTasks.append(task)
+
+	comTasks = []
+	for task in tasks:
+		if task.status == 'com':
+			comTasks.append(task)
+
+	incomTasks = []
+	for task in tasks:
+		if task.status == 'incom':
+			incomTasks.append(task)
+
+
 	context = {
-		"tasks": Task.objects.all(),
+	'todoTasks': todoTasks,
+	'comTasks': comTasks,
+	'incomTasks': incomTasks,
 	}
+
+	if (request.method == 'POST'):
+		print(request.POST)
 
 	return render(request, "tasks.html", context)
