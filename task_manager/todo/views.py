56c51bf56b3a253c2211cea701dc61292bdf0ec2
Sudeep Bhandari <sudeephb19@gmail.com>
2018-05-31 21:29:12 +0545

    'Add task' done

--- task_manager/todo/views.py
+++ task_manager/todo/views.py
@@ -16,10 +16,17 @@ def tasks(request):
 		if completed_task_id:
 		 	# print(f"Completed task id : {request.POST.get('todo')}")
 		 	completed_task = Task.objects.get(id = completed_task_id)
-		 	print(completed_task.status)
+		 	# print(completed_task.status)
 		 	completed_task.status = Status.objects.get(id = 3)		# completed has id 3 
-		 	print(f"{completed_task.status} - {completed_task}")
+		 	# print(f"{completed_task.status} - {completed_task}")
 		 	completed_task.save()
+
+	new_task = request.POST.get('newTask')
+	if new_task:
+		print(new_task)
+		s = Status.objects.create(status_text = 'to')
+		task = Task.objects.create(name = new_task, status = s)
+		task.save()
 
 
 	return render(request, "tasks.html", context)
