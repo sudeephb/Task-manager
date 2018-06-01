57131c4fd3d823bb7c10c903ac0fd6d9bea8562b
Sudeep Bhandari <sudeephb19@gmail.com>
2018-06-01 17:36:16 +0545

    Done! button works exactly like it's supposed to

--- task_manager/todo/views.py
+++ task_manager/todo/views.py
@@ -13,14 +13,13 @@ def tasks(request):
 
 	if (request.method == 'POST'):
 		remove_id = request.POST.get('remove')
-		completed_task_id = request.POST.get('todo');
+		completed_task_id = request.POST.get('done');
 		if completed_task_id:
-		 	# print(f"Completed task id : {request.POST.get('todo')}")
-		 	completed_task = Task.objects.get(id = completed_task_id)
-		 	# print(completed_task.status)
-		 	completed_task.status = Status.objects.get(id = 3)		# completed has id 3 
-		 	# print(f"{completed_task.status} - {completed_task}")
-		 	completed_task.save()
+		  	completed_task = Task.objects.get(id = completed_task_id)
+		#  	# print(completed_task.status)
+		  	completed_task.status = Status.objects.get(id = 3)		# completed has id 3 
+		#  	# print(f"{completed_task.status} - {completed_task}")
+		  	completed_task.save()
 
 		new_task = request.POST.get('newTask')
 		if new_task:
